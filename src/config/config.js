52c0b12770045f6c600c4806853c8a51dc5cb5c4
Matias Pedernera <matias.pedernerax8@gmail.com>
2020-04-08 23:47:30 -0300

    Primera versión de API

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-const config = {
+const configuration = {
     PORT : 3000,
     DDBB : {
         NAME : 'BddConDatos',
@@ -9,4 +9,4 @@ const config = {
     }
 }
 
-module.exports = {config}
+module.exports = {configuration}
